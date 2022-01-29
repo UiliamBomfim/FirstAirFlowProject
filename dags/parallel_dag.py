f467a2f83f44ae670c81777ff1e0e0f7bd126072
uiliam bomfim <wf3@outlook.com.br>
2022-01-28 23:02:21 -0300

    add x_comms and triggers rules

--- dags/parallel_dag.py
+++ dags/parallel_dag.py
@@ -1,7 +1,9 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.operators.subdag import SubDagOperator
+from airflow.utils.task_group import TaskGroup
 
-
+from subdags.subdag_parallel_dag import subdag_parallel_dag
 from datetime import datetime
 
 
@@ -19,19 +21,27 @@ with DAG('parallel_dag', schedule_interval='@daily',
         bash_command='sleep 3'
     )
 
-    task_2 = BashOperator(
+    with TaskGroup('processing_tasks') as processing_tasks:
+        task_2 = BashOperator(
         task_id='task_2',
         bash_command='sleep 3'
-    )
+        )
 
-    task_3 = BashOperator(
-        task_id='task_3',
-        bash_command='sleep 3'
-    )
+        with TaskGroup('spark_tasks') as spark_tasks:
+            task_3 = BashOperator(
+                task_id='task_3',
+                bash_command='sleep 3'
+            )
+
+        with TaskGroup('flink_tasks') as flink_tasks:
+            task_3 = BashOperator(
+                task_id='task_3',
+                bash_command='sleep 3'
+            )         
 
     task_4 = BashOperator(
         task_id='task_4',
         bash_command='sleep 3'
     )
 
-    task_1 >> [task_2, task_3] >> task_4
+    task_1 >> processing_tasks >> task_4
